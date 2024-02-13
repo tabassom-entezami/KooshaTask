be0e61fb6294a7c5ff12b24fd6e163bd4b8362b4
tabassom entezami <tabassom.entezami@gmail.com>
2024-02-13 19:53:50 +0330

    debug url slug

--- KooshaApp/urls.py
+++ KooshaApp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from KooshaApp.views import ShortenUrl, GetOriginalUrl
 
 urlpatterns = [
-    path('<str:short_url>', GetOriginalUrl.as_view(), name='get_original_url'),
+    path('<slug:short_url>', GetOriginalUrl.as_view(), name='get_original_url'),
     path('shorten-url/', ShortenUrl.as_view(), name='shorten_url'),
 ]
