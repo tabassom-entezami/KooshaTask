e5689374577dc2297aeb9eb5cdb9f9cfba12d887
tabassom entezami <tabassom.entezami@gmail.com>
2024-02-13 14:31:52 +0330

    create admin

--- KooshaApp/admin.py
+++ KooshaApp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from KooshaApp.models import ShortUrlModel
+
+
+@admin.register(ShortUrlModel)
+class ShortUrlAdmin(admin.ModelAdmin):
+    list_display = ('id', 'short_url', 'long_url', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('long_url',)
+    ordering = ('-created_at',)
+
