a5afa5d5126158bff4f1af75b43f8fd75e912be6
tabassom entezami <tabassom.entezami@gmail.com>
2024-02-13 18:40:37 +0330

    create short url and save

--- KooshaApp/views.py
+++ KooshaApp/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import ShortUrlModel
+from .serializers import ShortUrlSerializer
+from django.shortcuts import redirect
 
-# Create your views here.
+
+class ShortenUrl(APIView):
+
+    def post(self, request):
+        data = {
+            'long_url': request.data.get('url'),
+            'short_url': ShortUrlModel.generate_short_url(),
+        }
+
+        serializer = ShortUrlSerializer(data=data)
+
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+
